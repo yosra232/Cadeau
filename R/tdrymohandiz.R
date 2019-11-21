7ddce15a1f28b94c527cb51e1946baf36fe19335
yosra232 <moh.yosra@hotmail.com>
2019-11-21 13:02:49 +0100

    cadeaufinal

---
+++ R/tdrymohandiz.R
@@ -0,0 +1,9 @@
+cadeau <- function(prenom){
+  if ( prenom == "Marco" || penom == "marco")
+
+    paste ("Bonjour", Marco, "franchement j'ai pas d'inspiration " )
+}
+else {
+  paste (prenom, "je peux pas dire plus")
+
+}
